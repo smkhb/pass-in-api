6e693ed2d4d94bdae3909da92b9548150b4c6689
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-10-18 13:23:16 -0300

    Criando rotas

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,34 @@
-import fastify from "fastify";
+import fastify from "fastify"
+import { z } from "zod"
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+
+const prisma = new PrismaClient({log: ['query']});
+
+app.post('/events',  async (request, reply)=> {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  })
+
+  const data = createEventSchema.parse(request.body)
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees,
+      slug: new Date().toISOString()
+    }
+  })
+
+  return reply.status(201).send({ eventId: event.id }) 
+});
 
 app.listen({port : 3333})
   .then(()=> console.log("Server is running on port 3333"));
 
-app.get('/', ()=> "Hello World");
+
+
