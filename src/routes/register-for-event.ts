94e9407464e1bc7899008cb8bab04149b2a645ed
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-10-18 15:59:31 -0300

    Adicionando limitações
    Participante não pode se inscrever duas vezes no mesmo evento
    
    Evento que está cheio não aceita mais inscrição de participante

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -24,14 +24,45 @@ export async function registerForEvent(app: FastifyInstance) {
     }, async (request, reply) => {
       const { eventId } = request.params
       const { name, email } = request.body
-
-     const attendee = await prisma.attendee.create({
-        data: {
-          name,
-          email,
-          eventId
+     
+      const attendeeFromEmail = await prisma.attendee.findUnique({
+        where: {
+          eventId_email: {
+            email,
+            eventId
+          }
         }
-     })
+      })
+
+      if (attendeeFromEmail !== null) {
+        throw new Error("An attendee with the same email already exists")
+      }
+
+      const [event, amountOfAttendeesForEvent] = await Promise.all([
+        prisma.event.findUnique({
+          where: {
+            id: eventId
+          }
+        }),
+
+        prisma.attendee.count({
+          where: {
+            eventId
+          }
+        })
+      ])
+
+      if (event?.maximumAttendees && amountOfAttendeesForEvent >= event.maximumAttendees) {
+        throw new Error("Event is full")
+      }
+
+      const attendee = await prisma.attendee.create({
+          data: {
+            name,
+            email,
+            eventId
+          }
+      })
 
       return reply.status(201).send({ attendeeId: attendee.id })
     })
