62b64b0956937099fdf15dae0a825fa30597de8e
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-10-25 15:26:54 -0300

    Fakerjs | Prisma Seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,40 @@
 import { prisma } from '../src/lib/prisma'
+import { faker } from '@faker-js/faker'
 
 async function seed() {
-  await prisma.event.create({
+  const event = await prisma.event.create({
     data: {
       id: '2c6b43eb-bf38-4f0a-b868-081fa9ec3f27',
-      title: 'Event 1',
-      slug: 'event-1',
-      details: 'Event 1 Description',
-      maximumAttendees: 100,
+      title: 'Show do Bruno Mars',
+      slug: 'show-do-bruno-mars',
+      details: 'Show do Bruno Mars em Jurere Internacional',
+      maximumAttendees: 4000,
      
     }
   })
+
+  const attendees = Array.from({ length: 400 }, () => {
+    return {
+      name: faker.person.firstName(),
+      email: faker.internet.email(),
+      createAt: faker.date.recent({ days: 30 }),
+      eventId: event.id,
+    }
+  })
+
+  const promises = attendees.map((attendee) =>
+    prisma.attendee.create({data: attendee})
+    .then((createdAttendee) =>
+      prisma.checkIn.create({
+        data: {
+          createdAt: faker.date.recent({ days: 7 }),
+          attendeeId: createdAttendee.id,
+        }
+      })
+    )
+  )
+
+  await Promise.all(promises)
 }
 
 seed().then(() => {
